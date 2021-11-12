788df28e0489696675c2fc34869bfa9384f12198
AsmaSimreen <asmasimreen1001@gmail.com>
2021-11-12 23:51:43 +0530

    Event Handler and Hooks are added

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -1,15 +1,18 @@
+import { useState } from "react";
 import { bookables } from "../../static.json";
 
 export default function BookablesList() {
   const group = "Rooms";
   const bookablesInGroup = bookables.filter((b) => b.group === group);
-  const bookableIndex = 1;
+  const [bookableIndex, setBookableIndex] = useState(1);
 
   return (
     <ul className="bookables items-list-nav">
       {bookablesInGroup.map((b, i) => (
         <li key={b.id} className={i === bookableIndex ? "selected" : null}>
-          <button className="btn">{b.title}</button>
+          <button className="btn" onClick={() => setBookableIndex(i)}>
+            {b.title}
+          </button>
         </li>
       ))}
     </ul>
