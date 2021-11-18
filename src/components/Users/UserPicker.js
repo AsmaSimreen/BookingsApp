d31a23a829914fa8b61dbb1fc45f3dc28f81119a
AsmaSimreen <asmasimreen1001@gmail.com>
2021-11-18 21:33:10 +0530

    Working on UserPicker

--- src/components/Users/UserPicker.js
+++ src/components/Users/UserPicker.js
@@ -1,6 +1,17 @@
 import { users } from "../../static.json";
+import Spinner from "../UI/Spinner";
 
 export default function UserPicker() {
+  const [user, setUsers] = useState(null);
+
+  useEffect(() => {
+    fetch("https://localhost:3001/users")
+      .then((resp) => resp.json())
+      .then((data) => setUsers(data));
+  }, []);
+  if (users === null) {
+    return <Spinner />;
+  }
   return (
     <select>
       {users.map((u) => (
